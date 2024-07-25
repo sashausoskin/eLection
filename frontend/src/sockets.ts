16ec9ceeca00d2e2ea7fbf0675a731ca7d7cf15f
Sasha Usoskin <sashausoskin@live.com>
2024-07-25 13:44:53 +0300

    FIX socket problem
    It should now in theory work

--- frontend/src/sockets.ts
+++ frontend/src/sockets.ts
@@ -1,19 +1,22 @@
 import { Socket, io } from 'socket.io-client'
 
 export const createQueueSocket = (userCode: string, lobbyCode: string): Socket =>
-	io(`${import.meta.env.VITE_BACKEND_URL}/queue`, {
+	io(`${import.meta.env.VITE_SOCKET_URL}/queue`, {
+		path: import.meta.env.VITE_SOCKET_PATH,
 		auth: { userCode, lobbyCode},
 		autoConnect: false,
 	})
 
 export const createViewerSocket = (lobbyCode : string, hostID : string) : Socket => 
-	io(`${import.meta.env.VITE_BACKEND_URL}/viewer`, {
+	io(`${import.meta.env.VITE_SOCKET_URL}/viewer`, {
 		auth: {lobbyCode, hostID },
+		path: import.meta.env.VITE_SOCKET_PATH,
 		autoConnect: false,
 	})
 
 export const createLobbySocket = (lobbyCode : string, participantID : string) : Socket => 
-	io(`${import.meta.env.VITE_BACKEND_URL}/lobby`, {
+	io(`${import.meta.env.VITE_SOCKET_URL}/lobby`, {
+		path: import.meta.env.VITE_SOCKET_PATH,
 		auth: {lobbyCode, participantID },
 		autoConnect: false,
 	})
