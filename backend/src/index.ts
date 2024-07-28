f3dbae587c606851a68bc81cfbb7b00e3f54effb
Sasha Usoskin <sashausoskin@live.com>
2024-07-28 15:52:33 +0300

    ADD .env-template update
    Also added PATH to the backend .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { cleanupRoutine } from './services/cleanupservice'
 import { server } from './util/server'
 import dotenv from 'dotenv'
 
-const PORT : number = 3000
+const PORT : number = Number(process.env.PORT) | 3000
 const CLEANUP_INTERVAL = process.env.CLEANUP_INTERVAL ? Number(process.env.CLEANUP_INTERVAL) : 1000*60*5
 
 dotenv.config()
