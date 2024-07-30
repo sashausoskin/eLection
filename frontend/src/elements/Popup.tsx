16e15ea5cc8773e7dd2bace4f5de724f2e9e2256
Sasha Usoskin <sashausoskin@live.com>
2024-07-30 14:50:06 +0300

    ADD popup animations

--- frontend/src/elements/Popup.tsx
+++ frontend/src/elements/Popup.tsx
@@ -2,6 +2,7 @@ import confirmIcon from '/img/icons/confirm.svg'
 import cancelIcon from '/img/icons/cancel.svg'
 import { useContext } from 'react'
 import { PopupContext } from '../Contexts'
+import { useTransition, animated } from '@react-spring/web'
 
 /**
  * A popup that covers the entire screen. Uses {@link PopupContext} to get information on what to show.
@@ -9,22 +10,36 @@ import { PopupContext } from '../Contexts'
 const Popup = () => {
     const {popupInfo, clearPopup} = useContext(PopupContext)
 
+    const popupBackgroundAnimation = useTransition(popupInfo, {
+        from: {opacity: 0},
+        enter: {opacity: 1},
+        leave: {opacity: 0}
+    })
+
+    const popupAnimation = useTransition(popupInfo, {
+        from: {opacity: 0, scale: 0},
+        enter: {opacity: 1, scale: 1},
+        leave: {opacity: 1, scale: 0}
+    })
+
     return <>
-        {popupInfo && <div className='popupBackground'>
-            <div className='popupContainer'>
-                <a className='popupText' data-testid='popup-text'>{popupInfo.message}</a>
-                <div className='buttonsContainer'>
-                    <button className='confirmButton' data-testid='confirm-button' onClick={() => {popupInfo.onConfirm?.(); clearPopup()}}>
-                        <img src={confirmIcon} />
-                    </button>
-                    {popupInfo.type === 'confirm' && 
-                        <button className='cancelButton' data-testid='cancel-button' onClick={() => {popupInfo.onCancel?.(); clearPopup()}}>
-                            <img src={cancelIcon} />
+        {popupBackgroundAnimation((bgStyle, bgItem) => (bgItem &&
+            <animated.div style={bgStyle} className='popupBackground'>
+            {popupAnimation((style, item) => (item &&
+                <animated.div style={style} className='popupContainer'>
+                    <a className='popupText' data-testid='popup-text'>{item?.message}</a>
+                    <div className='buttonsContainer'>
+                        <button className='confirmButton' data-testid='confirm-button' onClick={() => {item?.onConfirm?.(); clearPopup()}}>
+                            <img src={confirmIcon} />
                         </button>
-                    }
-                </div>
-            </div>
-        </div>}
+                        {item?.type === 'confirm' && 
+                            <button className='cancelButton' data-testid='cancel-button' onClick={() => {item?.onCancel?.(); clearPopup()}}>
+                                <img src={cancelIcon} />
+                            </button>
+                        }
+                    </div>
+            </animated.div>))}
+        </animated.div>))}
     </>
 }
 
