738d9a2f2f02e025797c8ba89dab9228f8814613
Sasha Usoskin <sashausoskin@live.com>
2025-08-08 14:13:21 +0300

    Refactored the host's user code input to also use OTP input

--- frontend/src/routes/host/Authentication.tsx
+++ frontend/src/routes/host/Authentication.tsx
@@ -1,7 +1,5 @@
-import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { auhtenticateUserWithCode } from '../../services/lobbyHostService'
-import * as Yup from 'yup'
-import { use, useEffect, useState } from 'react'
+import { use, useEffect, useRef, useState, useTransition } from 'react'
 import { ErrorMessage, StatusMessage } from '../../types'
 import { AxiosError } from 'axios'
 import { Mock } from 'vitest'
@@ -9,6 +7,8 @@ import { useTranslation } from 'react-i18next'
 import { PopupContext } from '../../context/Contexts'
 import { useNavigate } from 'react-router'
 import './Authentication.css'
+import { InputOtp } from 'primereact/inputotp'
+import { USER_CODE_LENGTH } from '../../util/config'
 
 /**
  * A form that allows the host to let people into the lobby.
@@ -24,7 +24,9 @@ export const Authentication = ({
 	onSubmitUserCode?: ((userCode: string) => never ) | Mock;
 }): React.ReactElement => {
 	const [statusMessage, setStatusMessage] = useState<StatusMessage | null>(null)
-	const statusMessageColor = statusMessage?.status === 'success' ? 'green' : 'red'
+	const [inputtedUserCode, setInputtedUserCode] = useState<string>('')
+	const [isCheckingUserCode, startUserCodeCheck] = useTransition()
+	const otpInputRef = useRef<HTMLDivElement>(null)
 
 	const {t} = useTranslation()
 	const {createPopup} = use(PopupContext)
@@ -43,30 +45,22 @@ export const Authentication = ({
 
 	}, [statusMessage])
 
-	const userCodeSchema = Yup.object({
-		userCode: Yup.string()
-			.required(t('fieldError.missingUserCode'))
-			.matches(/^[0-9]+$/, t('fieldError.onlyDigits'))
-			.min(4, t('fieldError.notValidUserCode'))
-			.max(4, t('fieldError.notValidUserCode')),
-	})
-
 	/**
 	 * Called when the host presses 'Submit' if {@link onSubmitUserCode} was not provided.
 	 * @param userCode - The user code that should be authenticated.
-	 * @param actions - Actions provided by Formik.
 	 */
 	const defaultOnSubmitUserCode = async (
 		userCode: string,
-		actions: FormikHelpers<{ userCode: string }>
 	) => {
+		if (userCode.length !== USER_CODE_LENGTH) return
+
 		try {
 			await auhtenticateUserWithCode(userCode)
 			setStatusMessage({
 				status: 'success',
 				message: t('status.userAuthenticated'),
 			})
-			actions.resetForm()
+			setInputtedUserCode('')
 		} catch (e) {
 			if (e instanceof AxiosError) {
 				switch((e.response?.data as ErrorMessage).type) {
@@ -75,6 +69,7 @@ export const Authentication = ({
 							status: 'error',
 							message: t('status.userNotFound'),
 						})
+						setInputtedUserCode('')
 						break
 					case 'UNAUTHORIZED':
 					// If the host is no longer authorized, it probably means that the lobby has closed and the lobby is redirected to the main menu.
@@ -87,50 +82,54 @@ export const Authentication = ({
 				}
 			}
 		}
+		if (otpInputRef.current?.firstChild instanceof HTMLInputElement) {
+			otpInputRef.current?.firstChild?.focus()
+		}
 	}
+
+	useEffect(() => {
+		if (inputtedUserCode.length !== USER_CODE_LENGTH) return
+
+		// Do not make multiple checks at once. This is also to avoid React Strict Mode's restrictions
+		if (isCheckingUserCode) return
+
+		startUserCodeCheck(async () => {
+			(onSubmitUserCode ? onSubmitUserCode : defaultOnSubmitUserCode)(inputtedUserCode)
+		})
+	}, [inputtedUserCode])
 
 	return (
 		<>
 			<h2>{t('lobbyCode')}:</h2>
 			<h2 className='lobbyCodeDisplay' data-testid="lobbycode">{lobbyCode}</h2>
 			<p>{t('hostInstructions.userAuthentication')}</p>
-			<Formik
-				initialValues={{ userCode: '' }}
-				validationSchema={userCodeSchema}
-				onSubmit={(values, actions) => {
-					if (onSubmitUserCode === undefined) {
-						defaultOnSubmitUserCode(values.userCode, actions)
-					}
-					else {
-						onSubmitUserCode(values.userCode)
-					}
-				}}
-			>
-				{({ errors, touched, isValid }) => (
-					<>
-						<div className='userCodeField'>
-							<Form className='userCodeField'>
-								<Field name="userCode" autoComplete='off' size={4} data-testid="usercode-field" />
-							
-								<button type="submit" disabled={!isValid} data-testid="submit-authentication">
-									{t('button.submit')}
-								</button>
-							</Form>
-						</div>
-						{errors.userCode && touched.userCode ? (
-							<a data-testid="usercode-field-error">{errors.userCode}</a>
-						) : null}
-						{statusMessage && (
-							<a
-								data-testid={`status-message-${statusMessage.status}`}
-								style={{ color: statusMessageColor }}
-							>
-								{statusMessage.message}
-							</a>
-						)}
-					</>
-				)}
-			</Formik>
+			<div className='userCodeField'>
+				<InputOtp
+					name="lobbyCode"
+					invalid={statusMessage !== null && statusMessage.status === 'error'}
+					value={inputtedUserCode}
+					length={USER_CODE_LENGTH}
+					integerOnly={true}
+					className='userCodeInput'
+					pt={{root: {'data-testid': 'usercode-field', ref: otpInputRef}}}
+					onChange={(event) => {
+						setStatusMessage(null)
+
+						if (!event.value) return
+						if (typeof event.value !== 'string') return
+						setInputtedUserCode(event.value)
+					}}
+					disabled={isCheckingUserCode}
+				/>
+			</div>
+			{statusMessage && (
+				<a
+					data-testid={`status-message-${statusMessage.status}`}
+					style={{ color: statusMessage.status === 'success' ? 'var(--confirm-button-color)' : 'var(--cancel-button-color)' }}
+				>
+					{statusMessage.message}
+				</a>
+			)}
 		</>
 	)
 }
