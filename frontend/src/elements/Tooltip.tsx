0d88bf0a5bf7a39699bec0f6ea324a5093dff48f
Sasha Usoskin <sashausoskin@live.com>
2026-05-09 12:18:21 +0300

    Updated react-tooltips to v6
    This should make working with tooltips easier and fix some visual bugs

--- frontend/src/elements/Tooltip.tsx
+++ frontend/src/elements/Tooltip.tsx
@@ -2,20 +2,19 @@ import { Tooltip } from 'react-tooltip'
 import { JSX } from 'react'
 import infoIcon from '/img/icons/info.svg'
 import './elements.css'
-import { renderToStaticMarkup } from 'react-dom/server'
 
 /**
  * An icon that shows elements when hovered over it. Mostly used as a tooltip.
  * @param children What the tooltip should show when hovered over it.
+ * @param id An ID to give to the tooltip. Required for tooltips to work correctly.
  * @returns 
  */
-const InfoTooltip = ({children} : {children : JSX.Element}) => {
-	// I don't know why, but the tooltip elements need an id or they don't work
-	const tooltipId = '123'
-
+const InfoTooltip = ({children, id} : {children : JSX.Element, id : string}) => {
 	return <>
-		<img className='icon tooltipIcon' src={infoIcon} width={20} height={20} data-tooltip-id={tooltipId} data-tooltip-html={renderToStaticMarkup(children)}/>
-		<Tooltip className='tooltip' id={tooltipId} />
+		<img className='icon tooltipIcon' src={infoIcon} width={20} height={20} data-tooltip-id={id}/>
+		<Tooltip className='tooltip' id={id}>
+			{children}
+		</Tooltip>
 	</>
 }
 export default InfoTooltip
