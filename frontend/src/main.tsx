3ab0b2d2e0980f64adab92993e5d78687c65f5f6
Sasha Usoskin <sasha.usoskin@sievo.com>
2026-09-16 17:19:50 +0300

    Upgraded React Router to v8

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 import { Globals } from '@react-spring/web'
 import { PopupContextProvider } from './context/ContextProviders.tsx'
 import './index.css'
